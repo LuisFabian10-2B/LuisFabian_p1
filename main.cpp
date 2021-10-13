3c9ec1e95fa546efaa29b60aaa2639e5b724af9d
LuisFabian2 <89663840+LuisFabian10-2B@users.noreply.github.com>
2021-10-13 15:47:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+  cout<< "Luis \n";//primer nombre/
+  cout<< "Fabian \n";//segundo nombre/
+  cout<< "Granados \n";//primer apellido/
+  cout<< "Castillo \n";//segundo apellido/
+  cout<< "10-2 B\n";}//sección/
